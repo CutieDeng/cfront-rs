09a375e354f1c509b336a8b3165ee0e4384682d2
CutieDeng <CutieDeng@iCloud.com>
2023-09-26 23:05:33 +0800

    Print simple log on bin.

--- cfront-definition-bin/src/main.rs
+++ cfront-definition-bin/src/main.rs
@@ -6,5 +6,16 @@ fn main() {
     stdin.read_line(&mut content).unwrap(); 
     let tokens = cfront_definition_lexer::analyze(&content); 
     let expressions = cfront_definition_parser::ast::compound::parse_expression(&tokens); 
-    println!("{:#?}", expressions);
+    // println!("{:#?}", expressions);
+    match expressions {
+        Ok((_, end)) => {
+            if end.is_empty() {
+                println!("A expression! ")
+            } else {
+                println!("Not a complete expression! ")
+            }
+        }
+        Err(_) => 
+            println!("Error in parsing! ")
+    }; 
 }
