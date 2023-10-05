80f059c5477b3d0b5eebdee8464a72a64593c89c
CutieDeng <CutieDeng@iCloud.com>
2023-10-05 10:57:18 +0800

    Impl api.

--- cfront-definition-parser/src/lib.rs
+++ cfront-definition-parser/src/lib.rs
@@ -3,9 +3,13 @@
 use ast::Ast;
 use cfront_definition::token::Token;
 
+use crate::ast::{translation_unit::TranslationUnit, AstType};
+
 pub fn parser<'a> (input: &'a [Token<'a>]) -> Result<Ast<'a>, ()> {
-    let _ = input; 
-    todo!()
+    let p = TranslationUnit::parse(&mut Vec::new(), input)?; 
+    let len = input.len() - p.1.len(); 
+    let a = Ast(AstType::TranslationUnit(p.0), &input[..len]); 
+    return Ok(a); 
 } 
 
 pub mod ast; 
